643f7cbdd921f184214623639f5185c4d380ca66
JP Saraceno <jp@scvsoft.com>
2020-01-16 17:32:45 -0300

    mueve la configuración a un .env

---
+++ screens/NotReady/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    padding: 10,
+    fontSize: 26
+  },
+  item: {
+    padding: 5,
+    fontSize: 16
+  }
+})
+
+export default styles
