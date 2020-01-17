d3f92fb8dc4443b2923f176e8e3599c3c404ac11
JP Saraceno <jp@scvsoft.com>
2020-01-17 10:53:48 -0300

    agrega prettier y se lo pasa a los archivos existentes

--- Home.tsx
+++ Home.tsx
@@ -1,44 +1,50 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { FloatingAction } from "react-native-floating-action";
-import Rankings from './Rankings';
-import PlayersAware, { PlayersAwareComponentProps } from './PlayersAware';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { FloatingAction } from 'react-native-floating-action'
+import Rankings from './Rankings'
+import PlayersAware, { PlayersAwareComponentProps } from './PlayersAware'
 
 const actions = [
   {
-    text: "Match",
-    name: "bt_match"
+    text: 'Match',
+    name: 'bt_match'
   }
-];
+]
 
-function Home({ players, navigation }: PlayersAwareComponentProps & { navigation: any }) {
+function Home({
+  players,
+  navigation
+}: PlayersAwareComponentProps & { navigation: any }) {
   return (
     <View style={styles.container}>
       <Rankings players={players} />
-      { players && <FloatingAction
-        actions={actions}
-        onPressItem={name => {
-          switch (name) {
-          case 'bt_match':
-            navigation.push('SelectPlayers', { players: players })
-            break;
-          }
-        }} /> }
+      {players && (
+        <FloatingAction
+          actions={actions}
+          onPressItem={name => {
+            switch (name) {
+              case 'bt_match':
+                navigation.push('SelectPlayers', { players: players })
+                break
+            }
+          }}
+        />
+      )}
     </View>
-  );
+  )
 }
 
 Home.navigationOptions = {
-  title: '#aca-se-duck-game-pato Ranking',
-};
+  title: '#aca-se-duck-game-pato Ranking'
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
 
 export default PlayersAware(Home)
--- Rankings.tsx
+++ Rankings.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
-import { getPlayers, PlayerData } from "./ranking";
-import { Text, View, Image, StyleSheet } from "react-native";
-import React from "react";
-import { FlatList } from "react-native-gesture-handler";
-import PlayersAware, { PlayersAwareComponentProps } from "./PlayersAware";
+import { useState, useEffect } from 'react'
+import { getPlayers, PlayerData } from './ranking'
+import { Text, View, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import { FlatList } from 'react-native-gesture-handler'
+import PlayersAware, { PlayersAwareComponentProps } from './PlayersAware'
 
 interface ItemProps {
   player: PlayerData
@@ -16,46 +16,78 @@ const styles = StyleSheet.create({
     height: 70,
     resizeMode: 'stretch'
   }
-});
+})
 
 const Item: React.FC<ItemProps> = ({ player, index }) => {
   return (
-    <View style={{
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderBottomColor: 'black',
-      borderBottomWidth: 0.33,
-    }}>
-      <Text style={{
-        padding: 10,
-        fontSize: 20 + (10 / index),
-        width: 50,
-        textAlign: 'center',
-      }}>{ index }</Text>
-      <Text style={{
-        flexGrow: 1,
-        fontWeight: 'bold',
-      }}>{ player.name }</Text>
-      <Image
-          style={ styles.stretch }
-          source={{ uri: player.hat }}
-        />
-      <Text style={{
-        width: 60,
-        textAlign: 'center',
-      }}>{ Number(player.rating).toFixed(0) }</Text>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomColor: 'black',
+        borderBottomWidth: 0.33
+      }}
+    >
+      <Text
+        style={{
+          padding: 10,
+          fontSize: 20 + 10 / index,
+          width: 50,
+          textAlign: 'center'
+        }}
+      >
+        {index}
+      </Text>
+      <Text
+        style={{
+          flexGrow: 1,
+          fontWeight: 'bold'
+        }}
+      >
+        {player.name}
+      </Text>
+      <Image style={styles.stretch} source={{ uri: player.hat }} />
+      <Text
+        style={{
+          width: 60,
+          textAlign: 'center'
+        }}
+      >
+        {Number(player.rating).toFixed(0)}
+      </Text>
     </View>
   )
 }
 
 export default function Rankings({ players }: PlayersAwareComponentProps) {
-  if (!players) { return <View><Text>Loading...</Text></View> }
-  return (<FlatList style={{
-    width: '100%',
-    flex: 1,
-  }} contentInset={{ top: 0, bottom: 85 }}
-    data={ players }
-    renderItem={ ({ item, index }) => <Item player={ item } index={ index + 1 } key={item.id} /> }
-    keyExtractor={ player => player.name }
-    ListHeaderComponent={<Image style={{height: 150, width: '100%', resizeMode: 'cover'}} source={{ uri: 'https://steamcdn-a.akamaihd.net/steam/apps/312530/capsule_616x353.jpg?t=1493313040' }} />} />)
+  if (!players) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+  return (
+    <FlatList
+      style={{
+        width: '100%',
+        flex: 1
+      }}
+      contentInset={{ top: 0, bottom: 85 }}
+      data={players}
+      renderItem={({ item, index }) => (
+        <Item player={item} index={index + 1} key={item.id} />
+      )}
+      keyExtractor={player => player.name}
+      ListHeaderComponent={
+        <Image
+          style={{ height: 150, width: '100%', resizeMode: 'cover' }}
+          source={{
+            uri:
+              'https://steamcdn-a.akamaihd.net/steam/apps/312530/capsule_616x353.jpg?t=1493313040'
+          }}
+        />
+      }
+    />
+  )
 }
